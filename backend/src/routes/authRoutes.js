3415a23174cce8b2ceac4e6ff52109ffd458e4cf
Dung <dung@gmail.com>
2025-11-30 10:51:48 +0700

    Hoàn thiện User (login,change-password) và CRUD cơ bản cho POST (bài đăng)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
+// 1. Import Middleware (đường dẫn tùy vào cấu trúc folder của bạn)
+const { verifyToken } = require("../middlewares/authMiddleware");
+
+// 2. Import Controller
 const {
   register,
   getAllUsers,
   login,
+  changePassword,
 } = require("../controllers/authController");
 
 router.post("/register", register);
-router.get("/users", getAllUsers);
+router.post("/login", login);
 
-// router.post('/login', login);
+router.get("/users", getAllUsers);
+// Route Đổi mật khẩu (BẮT BUỘC phải có verifyToken đứng trước)
+router.post("/change-password", verifyToken, changePassword);
 
 module.exports = router;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./authRoutes");
-// const postRoutes = require("./postRoutes");
+const postRoutes = require("./postRoutes");
+
 // const commentRoutes = require("./commentRoutes");
 
 router.use("/auth", authRoutes);
-// router.use("/posts", postRoutes);
+router.use("/posts", postRoutes);
+
 // // nest comments under posts
 // router.use("/posts/:postId/comments", commentRoutes);
 
