36138979dedc1694dd8ada00df15c75c44b82f3e
Dung <dung@gmail.com>
2025-12-10 22:46:57 +0700

    fix lỗi comment nằm trong post nền phải khau báo route tron post
    (cherry picked from commit b574425339be83c40fdacadc3465f7549fa5645c)

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -9,6 +9,7 @@ const {
   deletePost,
   likePost,
 } = require("../controllers/postController");
+const commentRoutes = require("./commentRoutes");
 const upload = require("../middlewares/upload");
 
 router.get("/", getPosts);
@@ -17,5 +18,8 @@ router.post("/", verifyToken, upload.single("image"), createPost);
 router.put("/:id", verifyToken, upload.single("image"), updatePost);
 router.delete("/:id", verifyToken, deletePost);
 router.put("/:id/like", verifyToken, likePost);
+
+// Nested comment routes
+router.use("/:postId/comments", commentRoutes);
 
 module.exports = router;
