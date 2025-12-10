b574425339be83c40fdacadc3465f7549fa5645c
Dung <dung@gmail.com>
2025-12-10 22:46:57 +0700

    fix lỗi comment nằm trong post nền phải khau báo route tron post

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,12 +3,8 @@ const router = express.Router();
 
 const authRoutes = require("./authRoutes");
 const postRoutes = require("./postRoutes");
-const commentRoutes = require("./commentRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/posts", postRoutes);
-
-// đặt comments dưới posts
-router.use("/posts/:postId/comments", commentRoutes);
 
 module.exports = router;
